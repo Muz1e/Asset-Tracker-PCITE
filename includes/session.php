d77d89a027016aab0d078be8ebb9ab183b1f30e1
Muz1e <71385330+Muz1e@users.noreply.github.com>
2022-10-19 00:16:19 +0800

    remove

--- includes/session.php
+++ includes/session.php
@@ -6,7 +6,7 @@ $user_check = $_SESSION['adminuid'];
 
 $ses_sql = mysqli_query($con,"SELECT `admin_uid` FROM `admin_acc` WHERE `admin_uid` = '$user_check' ");
 
-$row = mysqli_fetch_array($ses_sql,MYSQLI_ASSOC);
+$row = mysqli_fetch_assoc($ses_sql);
 
 $login_session = $row['admin_uid'];
 
