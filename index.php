6ff1c017d02279562e1e3b4b462052bef643060f
Muz1e <71385330+Muz1e@users.noreply.github.com>
2022-10-18 15:29:14 +0800

    Update index.php

--- index.php
+++ index.php
@@ -12,29 +12,29 @@
 
 <body>
 
-    <!-- <nav class="navbar navbar-inverse visible-xs">
-  <div class="container-fluid">
-    <div class="navbar-header">
-      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>                        
-      </button>
-      <a class="navbar-brand" href="#">Logo</a>
-    </div>
-    <div class="collapse navbar-collapse" id="myNavbar">
-      <ul class="nav navbar-nav">
-        <li class="active"><a href="#">Dashboard</a></li>
-        <li><a href="#">EVIDENCES</a></li>
-        <li><a href="#">VEHICLES</a></li>
-        <li><a href="#">WEAPONS</a></li>
-        <li><a href="#">IT EQUIPMENTS</a></li>
-        <li><a href="#">UNIFORMS</a></li>
-        <li><a href="#">REPORT</a></li>
-      </ul>
-    </div>
-  </div>
-</nav>
+    <nav class="navbar navbar-inverse visible-xs">
+        <div class="container-fluid">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="#">Logo</a>
+            </div>
+            <div class="collapse navbar-collapse" id="myNavbar">
+                <ul class="nav navbar-nav">
+                    <li class="active"><a href="#">Dashboard</a></li>
+                    <li><a href="#">EVIDENCES</a></li>
+                    <li><a href="#">VEHICLES</a></li>
+                    <li><a href="#">WEAPONS</a></li>
+                    <li><a href="#">IT EQUIPMENTS</a></li>
+                    <li><a href="#">UNIFORMS</a></li>
+                    <li><a href="#">REPORT</a></li>
+                </ul>
+            </div>
+        </div>
+    </nav>
 
     <div class="container-fluid">
         <div class="row content">
