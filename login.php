40a2f50fe40279d9105283719e01a56c4f6a50cf
Jimmark Daroy <111548265+jimmark1@users.noreply.github.com>
2022-10-18 15:31:28 +0800

    updated

--- login.php
+++ login.php
@@ -5,13 +5,44 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <? 
-        include 'includes/bootstrap_css_cdn.php'
-    ?>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="styles/login.css">
+
     <title>Login</title>
 </head>
 
 <body>
+    <div class="main-container p-5">
+        <div class="login-container p-5">
+            <div class="container shadow-lg" id="login-form">
+                <div class="logo text-center">
+                    <img src="images/pnp-logo.jpg" alt="pnp-logo">
+                </div>
+                <div class="form p-5 text-center">
+                    <div class="username-container mb-3 mt-3">
+                        <input type="text" class="username form-control" placeholder="username">
+                    </div>
+                    <div class="password-container mb-3 mt-3">
+                        <input type="password" class="username form-control" placeholder="password">
+                    </div>
+                    <div class="button-container">
+                        <button class="btn btn-primary btn-block">
+                            Login
+                        </button>
+                    </div>
+                </div>
+            </div>
+            <div class="container shadow-lg p-0">
+                <div class="bg-img-container">
+                    <img src="images/pnp.jpg" alt="bg-image">
+                </div>
+            </div>
+        </div>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous">
+    </script>
 
 </body>
 
