ce2869d596afeea53d99bda603d094966cda412d
Jimmark Daroy <111548265+jimmark1@users.noreply.github.com>
2022-10-19 08:05:24 +0800

    Update instaScan.js

--- js/instaScan.js
+++ js/instaScan.js
@@ -11,17 +11,3 @@ Instascan.Camera.getCameras()
   .catch(function (e) {
     console.log(e);
   });
-
-let return_scanner = new Instascan.Scanner({
-  video: document.getElementById("scanner-preview-return"),
-});
-
-Instascan.Camera.getCameras()
-  .then(function (cameras) {
-    cameras.length > 0
-      ? return_scanner.start(cameras[0])
-      : console.log("NO CAMERA FOUND");
-  })
-  .catch(function (e) {
-    console.log(e);
-  });
