1970b35e5c88807349371a2df37822ad6e395bee
Neo <lordneobarnachea@gmail.com>
2022-10-19 11:49:20 +0800

    added dashboard content

--- index.php
+++ index.php
@@ -10,7 +10,49 @@
 include 'includes/header.php';
 ?>
 <!-- dashboard main content start here -->
-
+<div class="col-sm-9">
+      <div class="well">
+        <h4>Dashboard</h4>
+        <p></p>
+      </div>
+      <div class="row">
+        <div class="col-sm-3">
+          <div class="well">
+            <h4>Evidence</h4>
+            <p>100</p> 
+          </div>
+        </div>
+        <div class="col-sm-3">
+          <div class="well">
+            <h4>Vehicles</h4>
+            <p>100</p> 
+          </div>
+        </div>
+        <div class="col-sm-3">
+          <div class="well">
+            <h4>Weapons</h4>
+            <p>100</p> 
+          </div>
+        </div>
+        <div class="col-sm-3">
+          <div class="well">
+            <h4>IT Equipment</h4>
+            <p>30</p> 
+          </div>
+        </div>
+        <div class="col-sm-3">
+          <div class="well">
+            <h4>Uniforms</h4>
+            <p>100</p> 
+          </div>
+        </div>
+        <div class="col-sm-3">
+          <div class="well">
+            <h4>Report</h4>
+            <p>100</p> 
+          </div>
+        </div>
+      </div>
 
 
 
